67a54352b3eaa20031e7da2e773dbcd447c45023
Norhan Salem <norhansalem51@gmail.com>
2024-01-03 01:23:27 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, {useState,useRef} from "react"
 import './App.css';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import squareCheck from '@fortawesome/free-solid-svg-icons'
+/* eslint-disable no-undef */
 
-function App() {
+function App() { 
+  const [Todo, setTodo] = useState([]);
+    const inputRef =useRef() 
+  const handelAddBtn=()=> {
+    //get value from input 
+    const text=inputRef.current.value
+    const newTask={completed :false ,text }
+    //set value to state  
+    setTodo([...Todo,newTask])
+    //clear input value 
+    inputRef.current.value=""
+  }
+  const handleTask=(index)=>{
+      const newToDo=[...Todo]
+      newToDo[index].completed=!newToDo[index].completed
+      setTodo(newToDo)
+  }
+  const handleDelete=(index)=>{
+    const newToDo=[...Todo]
+    newToDo.splice(index ,1 )
+    setTodo(newToDo)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2> My to do list </h2>
+      <input ref={inputRef} placeholder="enter task ..."/>
+      <button onClick={handelAddBtn}> Add </button>
+      <ul>
+      {Todo.map(({text,completed},index)=>{
+        return (
+          <div className="toDoLi">
+          <li className={completed? "done" :""} key={index} onClick={()=>handleTask(index)}>{text}</li>
+          <span onClick={()=>handleDelete(index)}>✖️</span>
+          </div>)
+        
+      })}
+      </ul>
+      
+      
     </div>
   );
 }
